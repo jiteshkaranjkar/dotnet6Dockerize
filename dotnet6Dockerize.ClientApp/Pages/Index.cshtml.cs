b088b9aa2a0da7a54ff2a77046573d4ac63cea92
Jitesh Karanjkar <JKaranjkar@rpmglobal.com>
2022-05-02 19:45:37 +1000

    Add project files.

---
+++ dotnet6Dockerize.ClientApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet6Dockerize.ClientApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            ViewData["Message"] = "Hello from webfrontend";
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://dotnet6Dockerize.WebAPI/WeatherForecast");
+                var response = await client.SendAsync(request);
+                ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
